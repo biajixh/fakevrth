bc26eb746c410f776ce94e59c836113c84d3a0a4
biajixh <xuhao@123>
2022-08-03 14:12:19 +0800

    [00003][学习开发]登录初步开发-web组件引入、跨域添加

--- src/main/java/com/biaji/learn/fakevrth/controller/LoginController.java
+++ src/main/java/com/biaji/learn/fakevrth/controller/LoginController.java
@@ -3,18 +3,26 @@ package com.biaji.learn.fakevrth.controller;
 import com.biaji.learn.fakevrth.dto.request.UserLogin;
 import com.biaji.learn.fakevrth.dto.response.Result;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 登录controller类
  */
-@Controller
+@RestController
 @Slf4j
 public class LoginController {
 
+    /**
+     * 登录api
+     * @param userLogin 用户信息请求
+     * @return Result 统一返回信息
+     */
     @PostMapping(value = "api/login")
-    @ResponseBody
-    public Result login(@ReseponseBody UserLogin userLogin){
-
+    @CrossOrigin
+    public Result login(@RequestBody UserLogin userLogin){
+        return new Result(200);
     }
 }
